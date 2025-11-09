4ab7e3af41cbbdf4b6b6a280e396fe23c947f0f9
Checamaro <checamar02@gmail.com>
2025-11-09 20:54:36 +0300

    Практика: Написание HTTP API клиента

---
+++ clients/http/gateway/cards/client.py
@@ -0,0 +1,32 @@
+from clients.http.client import HTTPClient
+from typing import TypedDict
+from httpx import Response
+
+
+class CreateCardsRequestDict(TypedDict):
+    """
+    Структура данных для создания новой виртуальной карты.
+    """
+    userId: str
+    accountId: str
+
+
+class CardsGatewayHTTPClient(HTTPClient):
+
+    def issue_virtual_card_api(self, request: CreateCardsRequestDict) -> Response:
+        """
+        Создание новой виртуальной карты.
+
+        :param request: Словарь с данными новой виртуальной карты.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post("/api/v1/cards/issue-virtual-card", json=request)
+
+    def issue_physical_card_api(self, request: CreateCardsRequestDict) -> Response:
+        """
+        Создание новой физической карты.
+
+        :param request: Словарь с данными новой физической карты.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post("/api/v1/cards/issue-virtual-card", json=request)
