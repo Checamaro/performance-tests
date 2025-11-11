d6e0d16b10d01ac5d7b98aeacbc61edd2270f067
Checamaro <checamar02@gmail.com>
2025-11-11 12:36:06 +0300

    Практикуемся в реализации HTTP API клиентов

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -3,9 +3,17 @@ from typing import TypedDict
 from httpx import Response
 
 
-class CreateCardsRequestDict(TypedDict):
+class CreateVirtualCardRequestDict(TypedDict):
     """
-    Структура данных для создания новой виртуальной/физической карты.
+    Структура данных для создания новой виртуальной карты.
+    """
+    userId: str
+    accountId: str
+
+
+class CreatePhysicalCardRequestDict(TypedDict):
+    """
+    Структура данных для создания новой физической карты.
     """
     userId: str
     accountId: str
@@ -16,7 +24,7 @@ class CardsGatewayHTTPClient(HTTPClient):
     Клиент для взаимодействия с /api/v1/cards сервиса http-gateway.
     """
 
-    def issue_virtual_card_api(self, request: CreateCardsRequestDict) -> Response:
+    def issue_virtual_card_api(self, request: CreateVirtualCardRequestDict) -> Response:
         """
         Создание новой виртуальной карты.
 
@@ -25,7 +33,7 @@ class CardsGatewayHTTPClient(HTTPClient):
         """
         return self.post("/api/v1/cards/issue-virtual-card", json=request)
 
-    def issue_physical_card_api(self, request: CreateCardsRequestDict) -> Response:
+    def issue_physical_card_api(self, request: CreatePhysicalCardRequestDict) -> Response:
         """
         Создание новой физической карты.
 
