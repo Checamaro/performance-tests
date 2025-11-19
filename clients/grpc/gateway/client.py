ecd2d0a51f409593c92900746799deb4a4f0cea9
Checamaro <checamar02@gmail.com>
2025-11-19 14:15:31 +0300

    Знакомство с gRPC API клиентами

---
+++ clients/grpc/gateway/client.py
@@ -0,0 +1,9 @@
+from grpc import Channel, insecure_channel
+
+
+def build_gateway_grpc_client() -> Channel:
+    """
+    Фабричная функция (билдер) для создания grpc-канала к сервису grpc-gateway.
+    :return: gRPC-канал (Channel), настроенный на localhost: 9003.
+    """
+    return insecure_channel("http://localhost:9003")
